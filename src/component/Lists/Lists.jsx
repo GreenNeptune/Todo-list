e44e3154a1e615655e1d5bdf154521886df11161
GreenNeptune <silentwisdom75@gmail.com>
2019-07-05 18:45:05 +0300

    setting inline headers

--- src/component/Lists/Lists.jsx
+++ src/component/Lists/Lists.jsx
@@ -9,15 +9,15 @@ export default class Lists extends Component {
         return (
             <div>
                 <div className="row border-black margin-top background-green">
-                    <div className=" d-flex col-2 offset-2">
+                    <div className=" d-flex col-2 offset-1">
                         <h3 className="deafault-list-header">Todo List</h3>
                     </div>
-                    <div className=" d-flex col offset-2">
+                    <div className=" d-flex col-2 offset-4">
                         <h3 className="deafault-list-header">Done List</h3>
                     </div>
                 </div>
                 <div className="row ToDo">
-                    <div className="d-flex align-items-center col-4 offset-2">
+                    <div className="d-flex align-items-center col-1">
                         <Collapsible open={true}>
                             <Scroll>
                                 {this.props.toDolist && this.props.toDolist.length > 0 ?
@@ -39,13 +39,13 @@ export default class Lists extends Component {
                         </Collapsible>
                         
                     </div>
-                    <div className="d-flex align-items-center col-4 ofset-2">
+                    <div className="d-flex align-items-center col-1 offset-5">
                         <Collapsible open={true}>
                             <Scroll>
-                                {this.props.doneList && this.props.doneList.length > 0 ?
+                                {this.props.toDolist && this.props.toDolist.length > 0 ?
                                     (
                                         <ul className="list-group">
-                                            {this.props.doneList.map((obj, index) =>
+                                            {this.props.toDolist.map((obj, index) =>
                                                 <Item
                                                     text={obj.text}
                                                     key={index}
