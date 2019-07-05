70fb6bba0727041f9ab077685b762319de44e185
yakir <yakirifrah19@gmail.com>
2019-07-05 10:22:30 +0300

    styling the item list

--- src/component/Item/Item.jsx
+++ src/component/Item/Item.jsx
@@ -4,12 +4,16 @@ import './item.css';
 const Item = ({ text }) => {
     return (
         <li className="list-group-item">
-            <input type="checkbox" />
-            <div className="icon favorite"></div>
-            <div className="icon trash"></div>
-            {text}
+            <div className="d-flex">
+                <input title="mark" type="checkbox" className="space-icon" />
+                <div title="favorite" className="icon favorite space-icon"></div>
+            </div>
+            <div className="d-flex">
+                {text}
+                <div title="delete" className="icon trash"></div>
+            </div>
         </li>
     );
-} 
+}
 
 export default Item;
