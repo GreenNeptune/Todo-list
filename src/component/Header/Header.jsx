0b90f277ca9e9cb97d3800d8c2eeb52efa6c5229
GreenNeptune <silentwisdom75@gmail.com>
2019-07-05 11:20:32 +0300

    setting app header layout

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import "./header.css";
 class Header extends Component {
     render() {
         return (
-            <div className="app-header">
-                <div className="app-header-title">{this.props.title}</div>
-                <div className="logo"></div>
+            <div className="row app-header">
+                <div className=" d-flex align-items-center col-4 offset-4">
+                    <div className="logo"></div>
+                    <div className="app-header-title">
+                        {this.props.title}
+                    </div>
+                </div>
             </div>
         )
     };
