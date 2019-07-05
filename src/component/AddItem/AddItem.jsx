7617ad872131a4d47fb7799620b4e17a3f7f5fa1
yakir <yakirifrah19@gmail.com>
2019-07-05 22:57:05 +0300

    solved conflicted

--- src/component/AddItem/AddItem.jsx
+++ src/component/AddItem/AddItem.jsx
@@ -22,14 +22,18 @@ class AddItem extends Component {
     }
     render() {
         return (
-            <div className="row add-item">
-                <div className="d-flex align-items-center col-8 offset-2">
+            <div className="row container-add-item">
+                <div className="col-6 offset-2">
                     <div>
-                        <input className="add-item-input" type="text" maxLength="65" onChange={this.handleChange} />
+                        <input className="add-item-input" type="text" maxlength="65" onChange={this.handleChange} />
                     </div>
+
+                </div>
+                <div className="col-2">
                     <div className="add-item-logo-container" onClick={this.handleSubmit}>
                         <div className="add-item-logo"></div>
                     </div>
+
                 </div>
             </div>
         )
