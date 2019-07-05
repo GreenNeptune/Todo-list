4c44243fb72f078f8c9b4b1f7c97c8eaea24f676
GreenNeptune <silentwisdom75@gmail.com>
2019-07-05 14:20:24 +0300

    styling the todlist and done list headers

--- src/component/Lists/Lists.jsx
+++ src/component/Lists/Lists.jsx
@@ -7,35 +7,38 @@ export default class Lists extends Component {
     render() {
         return (
             <div>
-                <div className="row">
-                    <div className="col offset-1 deafault-todo-list">Todo List</div>
+                <div className="row border-black margin-top">
+                    <div className="col offset-1">
+                        <h3 className="deafault-list-header">Todo List</h3>
+                    </div>
                 </div>
 
                 <div className="row ToDo">
                     <div className="d-flex align-items-center col-9 offset-1">
-                    <Collapsible trigger={'Todo List'}>
-                        {this.props.toDolist && this.props.toDolist.length > 0 ?
-                            (
-                                <ul className="list-group">
-                                    {this.props.toDolist.map((obj, index) =>
-                                        <Item
-                                            text={obj.text}
-                                            key={index}
-                                        // onCheckBoxClick={this.onCheckBoxClick}
-                                        />
-                                    )}
-                                </ul>
-                            )
-                            :
-                            <div></div>
-                        }
-                    </Collapsible>
+                        <Collapsible trigger={'Todo List'}>
+                            {this.props.toDolist && this.props.toDolist.length > 0 ?
+                                (
+                                    <ul className="list-group">
+                                        {this.props.toDolist.map((obj, index) =>
+                                            <Item
+                                                text={obj.text}
+                                                key={index}
+                                            // onCheckBoxClick={this.onCheckBoxClick}
+                                            />
+                                        )}
+                                    </ul>
+                                )
+                                :
+                                <div></div>
+                            }
+                        </Collapsible>
                     </div>
                 </div>
-                
-                <div className="row">
-                    <div className="deafault-done-list col offset-1">Done List</div>
 
+                <div className="row border-black margin-top">
+                    <div className="col offset-1">
+                        <h3 className="deafault-list-header">Done List</h3>
+                    </div>
                 </div>
                 <div className="row Done">
                     <div className="d-flex align-items-center col-9 offset-1">
