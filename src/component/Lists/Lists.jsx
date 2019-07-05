7a95cedf7b408e10b22018e1f2df820152ff3995
GreenNeptune <silentwisdom75@gmail.com>
2019-07-05 13:06:25 +0300

    centerign the tasks

--- src/component/Lists/Lists.jsx
+++ src/component/Lists/Lists.jsx
@@ -6,37 +6,50 @@ export default class Lists extends Component {
     render() {
         return (
             <div>
-                <div className="ToDo">
-                    {this.props.toDolist && this.props.toDolist.length > 0 ?
-                        (
-                            <ul className="list-group">
-                                {this.props.toDolist.map((obj, index) =>
-                                    <Item
-                                        text={obj.text}
-                                        key={index}
-                                    // onCheckBoxClick={this.onCheckBoxClick}
-                                    />
-                                )}
-                            </ul>
-                        )
-                        :
-                        <div className="deafault-todo-list">Todo List</div>
-                    }
+                <div className="row">
+                    <div className="col offset-1 deafault-todo-list">Todo List</div>
                 </div>
-                <div className="Done">
-                    {this.props.doneList && this.props.doneList.length > 0 ?
-                        (
-                            <ul>
-                                {this.props.doneList.map((obj, index) =>
-                                    <li key={index}>{`task: ${obj.text}`}</li>
-                                )}
-                            </ul>
-                        )
-                        :
-                        (
-                            <div className="deafault-done-list">Done List</div>
-                        )
-                    }
+
+                <div className="row ToDo">
+                    <div className="d-flex align-items-center col-9 offset-1">
+                        {this.props.toDolist && this.props.toDolist.length > 0 ?
+                            (
+
+                                <ul className="list-group">
+                                    {this.props.toDolist.map((obj, index) =>
+                                        <Item
+                                            text={obj.text}
+                                            key={index}
+                                        // onCheckBoxClick={this.onCheckBoxClick}
+                                        />
+                                    )}
+                                </ul>
+                            )
+                            :
+                            ""
+                        }
+                    </div>
+                </div>
+                
+                <div className="row">
+                    <div className="deafault-done-list col offset-1">Done List</div>
+
+                </div>
+                <div className="row Done">
+                    <div className="d-flex align-items-center col-9 offset-1">
+                        {this.props.doneList && this.props.doneList.length > 0 ?
+                            (
+                                <ul>
+                                    {this.props.doneList.map((obj, index) =>
+                                        <li key={index}>{`task: ${obj.text}`}</li>
+                                    )}
+                                </ul>
+                            )
+                            :
+                            ""
+
+                        }
+                    </div>
                 </div>
             </div>
         )
